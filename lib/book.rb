86f2d6bff4cf9f675ab614941a36118877702696
Nicholas Njeru <nicholasnjeru917@gmail.com>
2022-10-21 18:44:31 +0300

    completed assignment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,19 @@
+class Book
+attr_accessor :title, :author, :page_count, :genre
 
+
+def properties author, page_count, genre
+    @author = author
+    @page_count = page_count
+    @genre = genre
+end
+
+def initialize title 
+    @title = title 
+   
+end 
+def turn_page
+   puts "Flipping the page...wow, you read fast!"
+end
+end 
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,22 @@
 # Make your shoe class here!
+class Shoe
+attr_accessor :brand , :color , :size , :material , :condition
+
+def properties brand, color, size, material, condition
+@brand = brand
+@color = color
+@size = size
+@material = material
+@condition = condition
+end
+
+def cobble
+   puts 'says that the shoe has been repaired'
+   puts "Your shoe is as good as new!"
+   @condition = "new"
+end
+
+def initialize brand
+    @brand = brand
+end
+end
